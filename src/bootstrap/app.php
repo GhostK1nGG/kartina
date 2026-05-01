457e706b16c0c5db8b34fb6e0652558669d12d28
Трубчанинов Сергей Владиславович <trubchaninov@profdev.pro>
2026-05-01 15:22:11 +0700

    Trust reverse proxy headers for HTTPS

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
